caebe7f395a0910af79261a239cffe7be165ffac
Pallav Dubey <pallav.dubey@gmail.com>
2022-02-15 09:18:37 -0500

    update navbar

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
       <h1>SV Plant Watering</h1>
     </nav>
   );
